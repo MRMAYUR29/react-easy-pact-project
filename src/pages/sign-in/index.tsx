4c5a21323e12a6f37602da09354d59722ada72b3
Chirag Padhyal <55888522+MRMAYUR29@users.noreply.github.com>
2025-08-09 20:26:27 +0530

    login password eye changes

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -385,7 +385,7 @@ export const SignInPage = () => {
                         className="absolute right-3 top-3.5 text-gray-500 hover:text-gray-700"
                         onClick={() => setShowPassword(!showPassword)}
                       >
-                        {showPassword ? <FaRegEye /> : <FaEyeLowVision />}
+                        {showPassword ? <FaEyeLowVision />  :  <FaRegEye />}
                       </button>
                     </div>
 
